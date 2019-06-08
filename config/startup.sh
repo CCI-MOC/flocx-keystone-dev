997e62479bd85d295f16fd2ab8c6accfffcecc9e
Lars Kellogg-Stedman <lars@redhat.com>
2019-06-07 20:35:27 -0400

    use keystone-manage to test database availability
    we don't need the waitfordb.py script because we can just call
    `keystone-manage db_sync` in a loop until it succeeds to verify
    database availability.

--- config/startup.sh
+++ config/startup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 
 DTU='python /config/dtu.py'
-WAITFORDB='python /config/waitfordb.py'
 
 : ${KEYSTONE_DB_HOST:=database}
 export KEYSTONE_DB_HOST
@@ -11,17 +10,16 @@ $DTU -o /etc/keystone/keystone.conf /config/keystone.j2.conf
 $DTU -o /etc/httpd/conf.d/keystone-wsgi-main.conf /config/keystone-wsgi-main.j2.conf
 $DTU -o /root/clouds.yaml /config/clouds.j2.yaml
 
-echo "* waiting for database"
-$WAITFORDB --host ${KEYSTONE_DB_HOST} --user ${KEYSTONE_DB_USER} \
-	--password ${KEYSTONE_DB_PASSWORD} ${KEYSTONE_DB_NAME}
-
 echo "* initializing fernet tokens"
 keystone-manage fernet_setup \
 	--keystone-user keystone \
 	--keystone-group keystone
 
 echo "* initializing database schema"
-keystone-manage db_sync
+while ! keystone-manage db_sync; do
+	echo "! database schema initialization failed; retrying in 5 seconds..."
+	sleep 5
+done
 
 echo "* initializing service catalog"
 keystone-manage bootstrap \
