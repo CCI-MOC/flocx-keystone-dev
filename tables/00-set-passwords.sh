2344b2d6764c973f977f066fbc8cd2875130bdbc
Lars Kellogg-Stedman <lars@redhat.com>
2019-07-16 11:33:40 -0400

    Avoid permission problems when running database init scripts
    Rather than attempting to modify files in /docker-entrypoint-initdb.d,
    just wrap a call to mysql in a shell script so that we can directly
    use the necessary environment variables.
    
    Closes #4

--- tables/00-set-passwords.sh
+++
@@ -1,2 +0,0 @@
-sed -i -e "s/MARKET_DB_PASSWORD/$MARKET_DB_PASSWORD/g" /docker-entrypoint-initdb.d/create_user_db.sql
-sed -i -e "s/PROVIDER_DB_PASSWORD/$PROVIDER_DB_PASSWORD/g" /docker-entrypoint-initdb.d/create_user_db.sql
